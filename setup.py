aba570c0803e0cb4e10c1251a036307aad7be0c9
Giuseppe Di Lena <giuseppedilena92@gmail.com>
2020-08-05 16:32:11 +0200

    adding trevis and codecov files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CloudMeasurement',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Giuseppe1992/CloudMeasurement.git',
+    license='MIT',
+    author='Giuseppe Di Lena',
+    author_email='giuseppedilena92@gmail.com',
+    description='Cloud Measurement tool'
+)
