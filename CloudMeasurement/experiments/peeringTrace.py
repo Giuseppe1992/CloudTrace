1ee1ed7344999515206bac3b5a9f21598d576a4a
Giuseppe Di Lena <giuseppedilena92@gmail.com>
2020-08-06 11:20:46 +0200

    adding the general Architecture:
    - use liteSQL for persistent data
    - divided the main experiments in 3 different types
    - create a CLI for managing the experiments
    
    closes: #4
    closes: #8

--- CloudMeasurement/experiments/peeringTrace.py
+++ CloudMeasurement/experiments/peeringTrace.py
@@ -2,5 +2,6 @@ from .multiregionalTrace import MultiregionalTrace
 
 
 class PeeringTrace(MultiregionalTrace):
+    """peering experiment are similar to the multiregional trace, with the peering network"""
     pass
 
